c60d92a7cb5b00e2c5e6c6f11cabd2f316965158
rhino-22 <trimaiqbt@gmail.com>
2025-11-25 23:18:46 +0700

    fix

--- lib/Service/product_service.dart
+++ lib/Service/product_service.dart
@@ -21,7 +21,7 @@ class ProductService {
     String categoryId,
   ) async {
 
-    final url = Uri.parse("$serverUrl/categories/$categoryId");
+    final url = Uri.parse("$serverUrl/products/categories/$categoryId");
     final res = await http.get(url);
 
     if (res.statusCode == 200) {
